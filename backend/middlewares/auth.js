2e26ebde1ed2c0ca37e0b1adfae8558275c0d662
Nitish Minz <nitishkumar083@gmail.com>
2026-06-04 21:42:09 +0530

    changes in cookies handling

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,15 +5,15 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   // console.log(req.cookies.token)
-  // const { token } = req.cookies;
-  // console.log("token",token)
-  // if (!token) {
-  //   return next(new ErrorHandler("User Not Authorized", 401));
-  // }
-  // const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  const { token } = req.cookies;
+  console.log("token",token)
+  if (!token) {
+    return next(new ErrorHandler("User Not Authorized", 401));
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-  // req.user = await User.findById(decoded.id);
+  req.user = await User.findById(decoded.id);
 
-  // next();
+  next();
  
 });
