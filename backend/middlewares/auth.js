2b508d6f400ec02c3b20492b1674fead5943fdf2
Nitish Minz <nitishkumar083@gmail.com>
2026-06-04 21:39:38 +0530

    changes in cookies handling

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -15,25 +15,5 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   // req.user = await User.findById(decoded.id);
 
   // next();
-  // 1. Extract the token from cookies
-  const { token } = req.cookies;
-
-  // 2. Check if the token exists
-  if (!token) {
-    return next(new ErrorHandler("User Not Authorized", 401));
-  }
-
-  // 3. Verify the token
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-  // 4. Fetch the user and attach it to the request object
-  // (Excluding the password field from the fetched user object for security)
-  req.user = await User.findById(decoded.id).select("-password");
-
-  if (!req.user) {
-    return next(new ErrorHandler("User not found", 404));
-  }
-
-  // 5. Pass control to the next middleware/controller
-  next();
+ 
 });
