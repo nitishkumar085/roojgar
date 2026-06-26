60c9011213127102f7f80beed9ea2dec0abf1016
Nitish Minz <nitishkumar083@gmail.com>
2026-06-26 13:51:04 +0530

    logout issue

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin:['http://localhost:5173'],
+  origin:['http://localhost:5173','https://roojgar.onrender.com'],
   credentials:true
 }));
 
